4d8ca58e1dd95edff91a10261bd18a9e2fa67789
Jarvis <39226019+Jarfle8411@users.noreply.github.com>
2019-01-26 17:57:02 -0500

    Add files via upload
    I created a program which will open up notepad, once the user enters in the correct username and password.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System; 
+
+using System.Collections.Generic; 
+
+using System.Data.SqlClient; 
+
+using System.Linq; 
+
+using System.Text; 
+
+ 
+
+namespace YELLOW 
+
+{ 
+
+    class Program 
+
+    { 
+
+        static void Main(string[] args) 
+
+        { 
+
+            //create a SWL Connection with required connection string 
+
+            SqlConnection myConnection = new SqlConnection( 
+
+                "Trusted_Connection = ues;" + 
+
+                "Server=localhost;" + 
+
+                "Database = xyz;" + 
+
+                "connection timeout = 10"); 
+
+            try 
+
+            { 
+
+                myConnection.Open(); 
+
+                Console.WriteLine("myConnection to Database Sucessful...."); 
+
+            } 
+
+            catch (Exception e) 
+
+            { 
+
+                Console.WriteLine("There are connection errors..... \n=============================\n"); 
+
+                Console.WriteLine(e.Message); 
+
+            } 
+
+ 
+
+            finally 
+
+            { 
+
+                Console.WriteLine("Closing connection to data base....."); 
+
+                myConnection.Close(); 
+
+            } 
+
+ 
+
+        } 
+
+    } 
+
+} 
+
+ 
